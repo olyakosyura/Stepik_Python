409d15dfcf49331df847939949640b2394a332b3
Kosyura Olga <olyakosyura@gmail.com>
2017-04-14 17:57:38 +0300

    XML

--- Py2/3.7.1.py
+++ Py2/3.7.1.py
@@ -29,10 +29,17 @@ Sample Output:
 
 '''
 
-from xml.etree import ElementTree
-
-s= input()
-tree = ElementTree.parse(s)
+import xml.etree.ElementTree as ET
+ 
+cube_colors = {'red': 0, 'green': 0, 'blue': 0}
+ 
+def parse_cube(elem, deep_level=1):
+    if elem.findall('cube'):
+        for i in elem.findall('cube'):
+            parse_cube(i, deep_level+1)
+    cube_colors[elem.attrib['color']] += deep_level
+    
+tree = ET.ElementTree(ET.fromstring(input()))
 root = tree.getroot()
-
-print(root)
+parse_cube(root)
+print(cube_colors['red'], cube_colors['green'], cube_colors['blue'])
