9837fea49838f0ba5f0d6811883cb8f87c921c80
Kosyura Olga <olyakosyura@gmail.com>
2017-04-14 18:44:03 +0300

    regular expressions
    Part 2

---
+++ Py2/3.3.7.py
@@ -0,0 +1,25 @@
+'''
+
+Вам дана последовательность строк.
+В каждой строке замените первое вхождение слова, состоящего только из латинских букв "a" (регистр не важен), на слово "argh".
+
+Примечание:
+Обратите внимание на параметр count у функции sub﻿.
+Sample Input:
+There’ll be no more "Aaaaaaaaaaaaaaa"
+AaAaAaA AaAaAaA
+Sample Output:
+There’ll be no more "argh"
+argh AaAaAaA
+
+
+'''
+
+import sys
+import re
+a = r'\b[aA]+\b'
+b = 'argh'
+for line in sys.stdin:
+    line = line.rstrip()
+    print(re.sub(a,b, line, count=1))
+        
